62109b2cba3360cd06d94a8a53cc7aaa061ce727
YavorVladev <boxmmox@gmail.com>
2022-11-05 21:23:59 +0000

    changes
    made some changes

--- Python OOP/Encapsulation - Exercise/zoo.py
+++ Python OOP/Encapsulation - Exercise/zoo.py
@@ -1,3 +1,13 @@
+from animal import Animal
+from worker import Worker
+from caretaker import Caretaker
+from cheetah import Cheetah
+from keeper import Keeper
+from lion import Lion
+from tiger import Tiger
+from vet import Vet
+
+
 class Zoo:
     def __init__(self, name: str, budget: int, animal_capacity: int, worker_capacity: int):
         self.name = name
@@ -7,7 +17,126 @@ class Zoo:
         self.animals = []
         self.workers = []
 
-    def add_animal(self, animal: str, price: int):
-        pass
+    def add_animal(self, animal: Animal, price: int):
+        if self.__budget - price < 0:
+            return "Not enough budget"
+
+        if self.__animal_capacity == len(self.animals):
+            return "Not enough space for animal"
+
+        self.__budget -= price
+        self.animals.append(animal)
+
+        return f"{animal.name} the {animal.__class__.__name__}"
+
+    def hire_worker(self, worker: Worker):
+        if self.__worker_capacity == len(self.workers):
+            return "Not enough space for worker"
+
+        self.workers.append(worker)
+
+        return f"{worker.name} the {worker.__class__.__name__} hired successfully"
+
+    def fire_worker(self, worker_name):
+        try:
+            worker = next(filter(lambda w: w.name == worker_name, self.workers))
+        except StopIteration:
+            return f"There is no {worker_name} in the zoo"
+
+        self.workers.append(worker)
+
+        return f"{worker_name} fired successfully"
+
+    def pay_workers(self):
+        salaries = sum(w.salary for w in self.workers)
+
+        if self.__budget - salaries < 0:
+            return f"You have no budget to pay your workers. They are unhappy"
+
+        self.__budget -= salaries
+
+        return f"You payed your workers. They are happy. Budget left: {self.__budget}"
+
+    def tend_animals(self):
+        animal_cost = sum(a.money_for_care for a in self.animals)
+
+        if self.__budget - animal_cost < 0:
+            return f"You have no budget to tend the animals. They are unhappy"
+
+        self.__budget -= animal_cost
+
+        return f"You tended all the animals. They are happy. Budget left: {self.__budget}"
+
+    def profit(self, amount: int):
+        self.__budget += amount
+
+    def animals_status(self):
+        lions = list(filter(lambda a: a.__clas__.__name__ == "Lion", self.animals))
+        tigers = list(filter(lambda a: a.__clas__.__name__ == "Tiger", self.animals))
+        cheetahs = list(filter(lambda a: a.__clas__.__name__ == "Cheetah", self.animals))
+
+        result = [
+            f"You have {len(self.animals)} animals",
+            f"----- {len(lions)} Lions:"
+        ]
+        result.extend(lions)
+
+        result.append(f"----- {len(tigers)} Tigers:")
+        result.extend(tigers)
+
+        result.append(f"----- {len(cheetahs)} Cheetahs:")
+        result.extend(cheetahs)
+
+        return "\n".join(str(r) for r in result)
+
+    def workers_status(self):
+        info = {"Caretaker": [], "Vet": [], "Keeper": []}
+        [info[x._class__.__name__].append(str(x)) for x in self.workers]
+
+        result = [
+            f"You have {len(self.workers)} workers",
+            f"----- {len(info['Keeper'])} Keepers:",
+            *info['Keeper'],
+            f"----- {len(info['Caretaker'])} Caretakers:",
+            *info['Caretaker'],
+            f"----- {len(info['Vet'])} Vets:",
+            *info['Vet'],
+        ]
+
+        return '\n'.join(result)
 
 
+
+zoo = Zoo("Zootopia", 3000, 5, 8)
+
+# Animals creation
+animals = [Cheetah("Cheeto", "Male", 2), Cheetah("Cheetia", "Female", 1), Lion("Simba", "Male", 4), Tiger("Zuba", "Male", 3), Tiger("Tigeria", "Female", 1), Lion("Nala", "Female", 4)]
+
+# Animal prices
+prices = [200, 190, 204, 156, 211, 140]
+
+# Workers creation
+workers = [Keeper("John", 26, 100), Keeper("Adam", 29, 80), Keeper("Anna", 31, 95), Caretaker("Bill", 21, 68), Caretaker("Marie", 32, 105), Caretaker("Stacy", 35, 140), Vet("Peter", 40, 300), Vet("Kasey", 37, 280), Vet("Sam", 29, 220)]
+
+# Adding all animals
+for i in range(len(animals)):
+    animal = animals[i]
+    price = prices[i]
+    print(zoo.add_animal(animal, price))
+
+# Adding all workers
+for worker in workers:
+    print(zoo.hire_worker(worker))
+
+# Tending animals
+print(zoo.tend_animals())
+
+# Paying keepers
+print(zoo.pay_workers())
+
+# Fireing worker
+print(zoo.fire_worker("Adam"))
+
+# Printing statuses
+print(zoo.animals_status())
+print(zoo.workers_status())
