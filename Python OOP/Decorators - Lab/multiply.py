a918a9df79b15bb271f3e984ff6a4b6e8584fe87
YavorVladev <boxmmox@gmail.com>
2022-11-22 21:43:27 +0200

    Advanced Course
    Solved more OOP tasks.

---
+++ Python OOP/Decorators - Lab/multiply.py
@@ -0,0 +1,9 @@
+def multiply(times):
+    def decorator(func_ref):
+        def wrapper(*args):
+            result = func_ref(*args)
+            return result * times
+
+        return wrapper
+
+    return decorator
