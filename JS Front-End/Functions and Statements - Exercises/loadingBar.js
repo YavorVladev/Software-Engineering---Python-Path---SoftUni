c5cd942ecae2ea12c9db5ab4d5d1f8a41edfc5a5
YavorVladev <boxmmox@gmail.com>
2023-03-06 22:40:55 +0200

    JS Front-End
    ref

--- JS Front-End/Functions and Statements - Exercises/loadingBar.js
+++ JS Front-End/Functions and Statements - Exercises/loadingBar.js
@@ -1,23 +1,19 @@
-function loadingBar(percentage) {
-
-    let result = '';
-
-    switch (percentage) {
-        case 0 : result = "0% [..........]\nStill loading..."; break;
-        case 10 : result = "10% [%.........]\nStill loading..."; break;
-        case 20 : result = "20% [%%........]\nStill loading..."; break;
-        case 30 : result = "30% [%%%.......]\nStill loading..."; break;
-        case 40 : result = "40% [%%%%......]\nStill loading..."; break;
-        case 50 : result = "50% [%%%%%.....]\nStill loading..."; break;
-        case 60 : result = "60% [%%%%%%....]\nStill loading..."; break;
-        case 70 : result = "70% [%%%%%%%...]\nStill loading..."; break;
-        case 80 : result = "80% [%%%%%%%%..]\nStill loading..."; break;
-        case 90 : result = "90% [%%%%%%%%%.]\nStill loading..."; break;
-        case 100 : result = "100% Complete!\n[%%%%%%%%%%]"; break;
-
+function loadingBar(num) {
+  const percent = num / 10;
+  let bar = "[";
+  for (let i = 0; i < 10; i++) {
+    if (i < percent) {
+      bar += "%";
+    } else {
+      bar += ".";
     }
-
-    console.log(result);
+  }
+  bar += "]";
+  if (num < 100) {
+    console.log(`${num}% ${bar}`);
+    console.log("Still loading...");
+  } else {
+    console.log(`100% Complete!`);
+    console.log("[%%%%%%%%%%]");
+  }
 }
-
-loadingBar(100)
