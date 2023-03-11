16f15386bc088603b7e72e8701d6453c43a84d84
YavorVladev <boxmmox@gmail.com>
2023-03-11 19:54:41 +0200

    JS Front-End
    ref

--- JS Front-End/Objects and Classes - Lab/cats.js
+++ JS Front-End/Objects and Classes - Lab/cats.js
@@ -1,27 +1,25 @@
 function solve(data) {
-    let cats = [];
+  let cats = [];
 
-    class Cats {
-        constructor(name, age) {
-            this.name = name;
-            this.age = age;
-        }
-
-        meow() {
-            console.log(`${this.name}, age ${this.age} says Meow`);
-        }
-    }
-    for (let i = 0; i < data.length; i++) {
-        let [name, age] = data[i].split(' ');
-        let cat = new Cats(name, Number(age));
-        cats.push(cat);
+  class Cats {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
     }
 
-    for (const cat of cats) {
-        cat.meow()
-        
+    meow() {
+      console.log(`${this.name}, age ${this.age} says Meow`);
     }
+  }
+  for (let i = 0; i < data.length; i++) {
+    let [name, age] = data[i].split(" ");
+    let cat = new Cats(name, Number(age));
+    cats.push(cat);
+  }
 
+  for (const cat of cats) {
+    cat.meow();
+  }
 }
 
-solve(['Candy 1', 'Poppy 3', 'Nyx 2'])
+solve(["Candy 1", "Poppy 3", "Nyx 2"]);
