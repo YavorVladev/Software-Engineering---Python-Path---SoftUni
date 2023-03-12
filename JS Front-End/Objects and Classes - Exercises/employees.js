9c54354b4c7190292390a45b7991079514a71863
YavorVladev <boxmmox@gmail.com>
2023-03-12 09:58:58 +0200

    JS Front-End
    ref

---
+++ JS Front-End/Objects and Classes - Exercises/employees.js
@@ -0,0 +1,26 @@
+function solve(data) {
+
+    let allEmployees = [];
+    let employeeInfo = {};
+
+    for (const person of data) {
+
+        employeeInfo[person] = person.length;
+    }
+
+
+    for (const k in employeeInfo) {
+        console.log(`Name: ${k} -- Personal Number: ${employeeInfo[k]}`);
+    }
+
+
+
+}
+
+solve([
+    'Silas Butler',
+    'Adnaan Buckley',
+    'Juan Peterson',
+    'Brendan Villarreal'
+    ]
+    )
