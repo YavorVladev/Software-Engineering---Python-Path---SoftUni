bd4983a5ae4602ecc68106069e0a6a3cd10f140c
YavorVladev <boxmmox@gmail.com>
2023-03-14 13:54:01 +0200

    JS Front-End
    ref

---
+++ JS Front-End/Objects and Classes - Exercises/createDictoinary.js
@@ -0,0 +1,17 @@
+function createDictionary(input) {
+  const dictionary = {};
+
+  input.forEach((jsonString) => {
+    const obj = JSON.parse(jsonString);
+    const term = Object.keys(obj)[0];
+    const definition = obj[term];
+    dictionary[term] = definition;
+  });
+
+  const sortedTerms = Object.keys(dictionary).sort();
+
+  sortedTerms.forEach((term) => {
+    const definition = dictionary[term];
+    console.log(`Term: ${term} => Definition: ${definition}`);
+  });
+}
