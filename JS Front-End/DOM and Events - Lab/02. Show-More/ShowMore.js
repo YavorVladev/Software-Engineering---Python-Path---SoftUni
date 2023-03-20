943cca7c33b453ea1e1f9fb6b942984ddcc9c8e7
YavorVladev <boxmmox@gmail.com>
2023-03-20 22:59:04 +0200

    JS Front-End
    ref

---
+++ JS Front-End/DOM and Events - Lab/02. Show-More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    const innerText = document.getElementById("text");
+    const hideButton = document.getElementById("more");
+    innerText.style.display = "inline";
+    hideButton.style.display = "none";
+}
