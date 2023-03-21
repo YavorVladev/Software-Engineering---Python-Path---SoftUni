b98befa2d88fe4092b01469501304c9a44f5f0d2
YavorVladev <boxmmox@gmail.com>
2023-03-22 00:32:48 +0200

    JS Front-End
    ref

---
+++ JS Front-End/DOM and Events - Lab/06. Add-Delete/app.js
@@ -0,0 +1,20 @@
+function addItem() {
+  const liContainer = document.getElementById("items");
+  const userInput = document.getElementById("newItemText");
+
+  let newLi = document.createElement("li");
+  const newAnchor = document.createElement("a");
+
+  newLi.textContent = userInput.value;
+
+  newAnchor.textContent = "[Delete]";
+  newAnchor.addEventListener("click", deleteHandler);
+  newLi.appendChild(newAnchor);
+  liContainer.appendChild(newLi);
+  userInput.value = "";
+
+  function deleteHandler(event) {
+    const liItem = event.currentTarget.parentElement;
+    liItem.remove();
+  }
+}
