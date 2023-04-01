ac53357e94d351b7765a4fe4dd4ae2041513cb4a
YavorVladev <boxmmox@gmail.com>
2023-04-01 21:05:09 +0300

    JS Front-End
    ref

--- JS Front-End/DOM and Events - Exercises/04. Locked-Profile/app.js
+++ JS Front-End/DOM and Events - Exercises/04. Locked-Profile/app.js
@@ -1,3 +1,27 @@
 function lockedProfile() {
-    
+  // get all profile containers
+  const profiles = Array.from(document.querySelectorAll(".profile"));
+
+  // loop through each profile and attach click event listener to its button
+  profiles.forEach((profile) => {
+    const button = profile.querySelector("button");
+    button.addEventListener("click", () => {
+      const isLocked =
+        profile.querySelector('input[name$="Locked"]:checked').value === "lock";
+      const hiddenFields = profile.querySelector(
+        "#" + button.previousElementSibling.id
+      );
+
+      // if profile is locked, do nothing
+      if (isLocked) {
+        return;
+      }
+
+      // otherwise, toggle visibility of hidden fields
+      hiddenFields.style.display =
+        hiddenFields.style.display === "none" ? "block" : "none";
+      button.textContent =
+        hiddenFields.style.display === "none" ? "Show more" : "Hide it";
+    });
+  });
 }
