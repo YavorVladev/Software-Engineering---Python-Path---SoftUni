fc10a2dcb0dbfba19a643a4951c0b6a6711c8f7b
YavorVladev <boxmmox@gmail.com>
2023-04-04 17:04:05 +0300

    JS Front-End
    ref

--- JS Front-End/DOM and Events - Exercises/05. Fill-Dropdown/dropdown.js
+++ JS Front-End/DOM and Events - Exercises/05. Fill-Dropdown/dropdown.js
@@ -1,4 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
-    
+  let newItemText = document.getElementById("newItemText").value;
+  let newItemValue = document.getElementById("newItemValue").value;
+  let menu = document.getElementById("menu");
+  let option = document.createElement("option");
+  option.textContent = newItemText;
+  option.value = newItemValue;
+  menu.appendChild(option);
+  document.getElementById("newItemText").value = "";
+  document.getElementById("newItemValue").value = "";
 }
