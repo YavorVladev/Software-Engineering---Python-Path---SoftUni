259163d2e051fa731d218cc3b18b7b8fd7d7a378
YavorVladev <boxmmox@gmail.com>
2023-04-12 23:31:33 +0300

    JS Front-End
    ref

---
+++ JS Front-End/Exam Preparation/01. Hotel reservation-Resources/app.js
@@ -0,0 +1,128 @@
+window.addEventListener("load", solve);
+
+function solve() {
+
+    const inputDOMSelectors = {
+        firstName: document.getElementById("first-name"),
+        lastName: document.getElementById("last-name"),
+        checkIn: document.getElementById("date-in"),
+        checkOut: document.getElementById("date-out"),
+        numberGuests: document.getElementById("people-count")
+    };
+
+    const otherDOMSelectors = {
+        nextBtn: document.getElementById("next-btn"),
+        liContainer: document.querySelector(".info-list"),
+        confirmContainer: document.querySelector(".confirm-list"),
+    };
+
+    const inputRefData = {
+        firstName: null,
+        lastName: null,
+        checkIn: null,
+        checkOut: null,
+        numberGuests: null
+    };
+
+    otherDOMSelectors.nextBtn.addEventListener('click', nextEventHandler);
+
+    function nextEventHandler(e) {
+        e.preventDefault();
+        
+
+        const allInputsAreValid = Object.values(inputDOMSelectors).every((input) => input.value !== "");
+
+        if (!allInputsAreValid) {
+            return;
+        }
+
+
+        
+        const {firstName, lastName, checkIn, checkOut, numberGuests} = inputDOMSelectors
+        const liElement = createElement('li', otherDOMSelectors.liContainer, null, ['reservation-content']);
+        const articleElement = createElement('article', liElement);
+        createElement('h3', articleElement, `Name: ${firstName.value} ${lastName.value}`);
+        createElement('p', articleElement, `From date: ${checkIn.value}`);
+        createElement('p', articleElement, `To date: ${checkOut.value}`);
+        createElement('p', articleElement, `For ${numberGuests.value} people`);
+
+        const editBtn = createElement('button', liElement, 'Edit', ['edit-btn']);
+        const continueBtn = createElement('button', liElement, 'Continue', ['continue-btn']);
+
+        otherDOMSelectors.liContainer.appendChild(liElement);
+
+        for (const key in inputDOMSelectors) {
+            inputRefData[key] = inputDOMSelectors[key].value;
+        }
+
+        e.currentTarget.disabled = true; // disables the button
+        clearAllInputs();
+
+        editBtn.addEventListener('click', editEventHandler);
+        continueBtn.addEventListener('click', () => {
+
+            const dataRef = editBtn.parentNode;
+            console.log(dataRef);
+
+        });
+
+    }
+
+
+    function editEventHandler() {
+        for (const key in inputDOMSelectors) {
+            inputDOMSelectors[key].value = inputRefData[key];
+        }
+
+        otherDOMSelectors.liContainer.innerHTML = "";
+        otherDOMSelectors.nextBtn.disabled = false;
+
+
+    }
+
+    function clearAllInputs() {
+        Object.values(inputDOMSelectors).forEach((input) => {
+            input.value = '';
+        })
+    }
+
+
+
+
+    function createElement(type, parentNode, content, classes, id, attributes, useInnerHtml) {
+        const htmlElement = document.createElement(type);
+    
+        if (content && useInnerHtml) {
+          htmlElement.innerHTML = content;
+        } else {
+          if (content && type !== "input") {
+            htmlElement.textContent = content;
+          }
+    
+        } if (content && type === "input") {
+          htmlElement.value = content;
+        }
+    
+        if (classes && classes.length > 0) {
+          htmlElement.classList.add(...classes);
+        }
+    
+        if (id) {
+          htmlElement.id = id;
+        }
+    
+        if (attributes) {
+          for (const key in attributes) {
+            htmlElement[key] = attributes[key];
+          }
+        }
+    
+        if (parentNode) {
+          parentNode.appendChild(htmlElement);
+        }
+    
+        return htmlElement
+    
+    
+      }
+}
